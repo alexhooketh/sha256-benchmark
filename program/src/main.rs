417e0206e34f48430c7ad163532810de85393676
jordan <41rjordan@proton.me>
2024-07-23 15:02:39 +0400

    more flexible settings

--- program/src/main.rs
+++ program/src/main.rs
@@ -6,12 +6,13 @@ use sha2::{Digest, Sha256};
 pub fn main() {
     let mut hodgepodge = [0u8; 32];
 
-    for _ in 0..64 {
+    let hash_count: u32 = sp1_zkvm::io::read();
+    for _ in 0..hash_count {
         let mut h = Sha256::new();
         h.update(sp1_zkvm::io::read_vec());
-        let hash = h.finalize();
+        let hash = h.finalize().to_vec();
         for i in 0..32 {
-            hodgepodge[i] ^= hash[i];
+            hodgepodge[i] += hash[i];
         }
     }
 
